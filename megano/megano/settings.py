202fcc4dae05f3fd2b11dfdba686625598ff2e84
Вероника <n1ka.dev@yandex.ru>
2021-09-19 16:05:12 +0300

    first commit

--- megano/megano/settings.py
+++ megano/megano/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import django_heroku
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -48,11 +48,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'megano.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'django-frontend')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -111,11 +110,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'django-frontend')
+STATICFILES_DIRS = [
+    "django-frontend/assets",
+]
+STATIC_URL = '/assets/'
+MEDIA_URL = '/uploads/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# Activate Django-Heroku.
-django_heroku.settings(locals())
+ALLOWED_HOSTS = ['*']
--- megano/megano/urls.py
+++ megano/megano/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+
+from .views import MainView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+                  path('', MainView.as_view(), name="main"),
+                  path('admin/', admin.site.urls),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
