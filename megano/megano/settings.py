6fd869f30ddadfab323f7b05e7e59423566c5821
Вероника <n1ka.dev@yandex.ru>
2021-09-21 21:26:38 +0300

    setting index end

--- megano/megano/settings.py
+++ megano/megano/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "django-frontend")
+STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 
 STATIC_URL = '/assets/'
 STATICFILES_DIRS = [
