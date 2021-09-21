52e074c8b0350d57e6857751c1e8a239b01a2641
Вероника <n1ka.dev@yandex.ru>
2021-09-21 20:33:48 +0300

    fix settings

--- megano/megano/settings.py
+++ megano/megano/settings.py
@@ -116,11 +116,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, "django-frontend")
 
 STATIC_URL = '/assets/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'django-frontend', 'assets')
+    os.path.join(BASE_DIR, 'django-frontend', 'assets'),
+    "django-frontend/assets",
+
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-ALLOWED_HOSTS = ['*']
 django_heroku.settings(locals())
